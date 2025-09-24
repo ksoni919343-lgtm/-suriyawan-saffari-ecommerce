72b63fb7893ca20e7a0fe41896f6860161a65e52
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-24 07:47:54 +0530

    Update googleSheets.ts

--- lib/googleSheets.ts
+++ lib/googleSheets.ts
@@ -1,16 +1,9 @@
 import { google } from 'googleapis';
 
-const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY!),
+const auth = new google.auth.JWT({
+  email: process.env.GOOGLE_SHEETS_API_KEY!,
+  key: process.env.GOOGLE_SHEETS_API_KEY!.split('\n').join('\n'),
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-export async function appendToSheet(data: string[]) {
-  const sheets = google.sheets({ version: 'v4', auth });
-  await sheets.spreadsheets.values.append({
-    spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID!,
-    range: 'Sheet1',
-    valueInputOption: 'RAW',
-    resource: { values: [data] },
-  });
-}
+export const googleSheets = google.sheets({ version: 'v4', auth });
